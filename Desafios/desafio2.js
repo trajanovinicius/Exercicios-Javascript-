921e3a98e39f5e314be222e0d992d604126fa3d1
Vinicius Trajano <vinicius.trajano@linkapi.com.br>
2022-02-03 17:00:57 -0300

    starting challenge two

---
+++ Desafios/desafio2.js
@@ -0,0 +1,18 @@
+//Exercício – Letras repetidas
+/*Neste exercício, temos um array composto de letras repetidas e uma letra isolada. Por exemplo, temos um array chamado vetor:
+let vetor = ["a", "b", "a", "a", "c", "b"]
+Aqui, temos:
+vetor[0] = "a";
+vetor[1] = "b";
+vetor[2] = "a";
+vetor[3] = "a";
+vetor[4] = "c";
+vetor[5] = "b";
+
+Sabemos que a única letra que não possui repetição está na posição [4], elemento “c”. Então, precisamos retornar este elemento. 
+Criaremos uma variável que terá o nosso vetor ordenado de forma crescente;
+Criaremos uma variável chamada odd, que receberá futuramente o valor isolado;
+Percorreremos o vetor ordenado;
+Armazenaremos em variáveis o elemento atual e o próximo elemento;
+Se o elemento atual for igual ao próximo, seguimos a iteração;
+Senão, a variável odd recebe o valor isolado.*/

4f299e62f50ecdcf64a7479a10565ebd83c2405c
Vinicius Trajano <vinicius.trajano@linkapi.com.br>
2022-02-04 10:37:44 -0300

    starting challenge three

---
+++ Desafios/desafio3.js
@@ -0,0 +1,15 @@
+//Exercício Rotacionando vetor
+
+/*Neste exercício, criaremos uma função que rotaciona o nosso vetor. Levaremos em consideração as letras do alfabeto, de A a E.
+
+let vetorAlfabeto = ["A", "B", "C", "D", "E"];
+O que queremos que aconteça é: rotacionar o vetor a quantidade de vezes indicada. Essa quantidade será representada pela letra Q.
+
+Caso o valor apresentado por Q for 1, o array deverá rotacionar um elemento:
+
+["E", "A", "B", "C", "D"]
+Para realizar esse exercício, você precisará:
+
+Criar uma função rotacao(), que receberá dois parâmetros. O vetor e a quantidade (V, Q);
+Adicione um índice para percorrer o vetor;
+Caso o vetor passado por parâmetro for um vetor vazio, retorne um vetor vazio.*/
